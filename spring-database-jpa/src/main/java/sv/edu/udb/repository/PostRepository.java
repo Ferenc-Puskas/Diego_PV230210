5da89ad2f23bb9af0d1603ff08174d082acef66f
Diego_Peña <144511752+Ferenc-Puskas@users.noreply.github.com>
2025-07-22 18:45:23 -0600

    Guia Practica 05

---
+++ spring-database-jpa/src/main/java/sv/edu/udb/repository/PostRepository.java
@@ -0,0 +1,33 @@
+package sv.edu.udb.repository;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+import sv.edu.udb.repository.domain.Post;
+
+import java.util.List;
+
+@Repository
+public class PostRepository {
+    @PersistenceContext
+    private EntityManager entityManager;
+    public List<Post> findAll() {
+        final String QUERY = "select p from Post p";
+        return entityManager
+                .createQuery(QUERY, Post.class)
+                .getResultList();
+    }
+    public Post findById(final Long id) {
+        return entityManager.find(Post.class, id);
+    }
+    @Transactional
+    public void save(final Post post) {
+        entityManager.persist(post);
+    }
+    public void delete(final Post post) {
+        entityManager.remove(post);
+    }
+}
+
