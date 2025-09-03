7a53eee18c6a26c496db263058b923ea7dfd5c29
Ferenc-Puskas <PV230210@alumno.udb.edu.sv>
2025-09-02 18:47:06 -0600

    Semana 8 - GUIA 7

---
+++ Guia5javax/src/main/java/com/example/guia5javax/Guia5javaxApplication.java
@@ -0,0 +1,13 @@
+package com.example.guia5javax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Guia5javaxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Guia5javaxApplication.class, args);
+    }
+
+}
---
+++ Guia5javax/src/test/java/com/example/guia5javax/Guia5javaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guia5javax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guia5javaxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
